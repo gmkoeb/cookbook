80b9c74e2440daa9ac4ecbab9cca734514b620e0
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-12-13 15:13:19 -0300

    Melhora feature de busca de pousadas via api

--- spec/requests/apis/recipes_api_spec.rb
+++ spec/requests/apis/recipes_api_spec.rb
@@ -7,12 +7,13 @@ describe 'Recipes API' do
                             password: '123456', role: :admin)
       user = User.create(email: 'user@email.com', password: '123456')
       recipe_type = RecipeType.create!(name: 'Sobremesa')
-      Recipe.create!(title: 'Brigadeiro', cook_time: 120, recipe_type_id: recipe_type.id, 
-                     ingredients: 'Leite Condensado, Nescau', instructions: 'Ferver no fogão até atingir o ponto certo', user_id: admin.id )
+      Recipe.create!( title: 'Brigadeiro', cook_time: 120, recipe_type_id: recipe_type.id, 
+                      ingredients: 'Leite Condensado, Nescau', instructions: 'Ferver no fogão até atingir o ponto certo', user_id: admin.id )
       
       get "/api/v1/recipes"
   
       expect(response.status).to eq 200
+
     end
   end
 end
